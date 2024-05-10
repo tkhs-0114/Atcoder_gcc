15170b7d6fbb85631bcdf246631f6326dce8db6e
tkhs-0114 <tkhs.mail0114@gmail.com>
2024-05-10 15:22:16 +0900

    make docker

--- mycode/main.c
+++ mycode/main.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main(void){
 
+    int A;
+    scanf("%d", &A);
+    for(int i=0; i<A; i++){
+        int B;
+        scanf("%d", &B);
+        printf("%d ", B*10);
+    }
+    printf("\n");
     return 0;
 }
