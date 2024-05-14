140656b8acec111ccdd75a8575aa2ff5934dfdd9
tkhs-0114 <tkhs.mail0114@gmail.com>
2024-05-14 22:02:46 +0900

    1st Contest

--- mycode/main.c
+++ mycode/main.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
+
 int main(void){
 
-    int A;
-    scanf("%d", &A);
-    for(int i=0; i<A; i++){
-        int B;
-        scanf("%d", &B);
-        printf("%d ", B*10);
-    }
-    printf("\n");
+    int N;
+    scanf("%d", &N);
+
+    printf("%d\n", N);
     return 0;
 }
---
+++ mycode/save.sh
@@ -0,0 +1,1 @@
+copy main.c code/main.c
